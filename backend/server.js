fbadff9ead2c69bbdfb123f789059e5a1ddbe8c8
Johannes Beiser <beiser@w11k.de>
2019-11-05 11:38:56 +0100

    working single word without double ignoring emty strings

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a0de648916c4c64461b6d01033abd3138299464f832e8efbba4ddb5b14bfa1e0
-size 6991
+oid sha256:4625486068f3e47f79ecca0d69f5bf8cfcd05b2d8ad1a84302b8e50c04d08015
+size 10279
