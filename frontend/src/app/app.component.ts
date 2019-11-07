1244a61782f74e026490a926e7b703689d04f9f1
Johannes Beiser <beiser@w11k.de>
2019-11-07 10:10:11 +0100

    wip

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:843551c3fd33320f912d6dfbdf5b8494c8b1867cd36a56d43bee3c3d18ecc9ae
-size 3727
+oid sha256:57300c49ebe49f2be432da47b8535a728919ac238affea037d72ee039c811699
+size 4038
