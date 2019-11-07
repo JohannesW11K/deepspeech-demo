96898b1a47a7298bed53690812b50777f64002e4
Johannes Beiser <beiser@w11k.de>
2019-11-07 15:48:20 +0100

    Websocket now streams data into Duplex

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:57300c49ebe49f2be432da47b8535a728919ac238affea037d72ee039c811699
-size 4038
+oid sha256:7856204810b3a757b370ce78ea66bbc4736cb41664d6cded6b09fa304220aee7
+size 4010
