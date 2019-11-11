11f3cc77616d91dcc56f94d1241ecf6b99a0f38f
Johannes Beiser <beiser@w11k.de>
2019-11-11 17:43:41 +0100

    WIP

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a61fc3335f40592b099eeb1120ddc9761633fa3721945fe0f23dc217ec62f23
-size 13377
+oid sha256:95a5e977a2551d30ea4335e3dae1c9be8bb795f0c61a2a6df0cbc8bae60c7529
+size 13114
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d8fe736df1f8ad1c03a4effaad50caea04fec3af50760910d4946b56f502134
-size 3743
+oid sha256:b37324433506854e28c34d6ef92def8a74166e9f44fb43d8cde05b5961e622dd
+size 3804
