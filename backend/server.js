0b2497c3821818ef6380119fc88caa9417c4b027
Johannes B <beiserjohannes@gmail.com>
2019-11-18 18:45:44 +0100

    wip formatting

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e593d767482f2eee10b68227ff3f9c471d3e09a08b2965c0237a346ec6a3728e
-size 7172
+oid sha256:55c966db8d90a8973ae276dee6b6c954fd52931e80a85716581e9dd7313dec31
+size 7063
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b413d09d65660760c02dda289fe32f9ff404fa20817fef3a57f2cc053a85289
-size 6254
+oid sha256:79e3b4d119b61e1e9d708e7ff1559c7049d2156a94000419b4eb2a1927a1fa72
+size 6225
